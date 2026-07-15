52734256096fe001a931c40e7e9ffd874ddaa20d
Georgi Brankovanov <georgi.brankovanov@gmail.com>
2026-07-15 13:09:57 +0300

    Make hash routing conditional for GitHub Pages only

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+
+const isGitHubPages = typeof window !== 'undefined' && (
+  window.location.host.includes('github.io') || window.location.pathname.startsWith('/nm-chronicles/')
+);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(
       routes,
+      ...(isGitHubPages ? [withHashLocation()] : []),
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
         anchorScrolling: 'enabled',
       })
-    ), provideClientHydration()
-  ]
+    ),
+    provideClientHydration(),
+  ],
 };
